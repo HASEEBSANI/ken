621846cd80b9a322a7b402ec8232c9a40506a1ae
haseebsani <kendailyreports@gmail.com>
2026-05-19 23:55:29 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,11 +1,8 @@
-const API_URL =
-"https://script.google.com/macros/s/AKfycbwxa4cEaMF2GV9dY4kjKxbB8wcJdWhPnQUoPYkysiguNVoc1LHxnrUQT9n_5qNBQ8IskQ/exec";
 
 async function getDashboardData() {
-
-  const res = await fetch(
-    `${API_URL}?action=dashboard`
+  const response = await fetch(
+    "https://script.google.com/macros/s/AKfycbwxa4cEaMF2GV9dY4kjKxbB8wcJdWhPnQUoPYkysiguNVoc1LHxnrUQT9n_5qNBQ8IskQ/exec"
   );
 
-  return await res.json();
+  return await response.json();
 }
